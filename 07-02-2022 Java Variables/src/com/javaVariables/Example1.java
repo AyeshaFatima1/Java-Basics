c3734e98f44211932480fb0bcd18bd665b72ccfe
AyeshaFatima1 <98936880+AyeshaFatima1@users.noreply.github.com>
2022-09-11 18:42:25 +0530

    Add files via upload

---
+++ 07-02-2022 Java Variables/src/com/javaVariables/Example1.java
@@ -0,0 +1,34 @@
+package com.javaVariables;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//Write a java program to print student information using java variables and literals
+		
+		//variable declaration
+		int studentId;
+		String studentName;
+		float studentMarks;
+		String studentAddress;
+		
+		//variable initialization
+		studentId=522;
+		studentName="Ayesha";
+		studentMarks=80.50f;
+		studentAddress="Hyderabad";
+		
+		System.out.print("student Id:");
+		System.out.println(studentId);
+		
+		System.out.print("student Name:");
+		System.out.println(studentName);
+		
+		System.out.print("student Marks:");
+		System.out.println(studentMarks);
+		
+		System.out.print("student Address:");
+		System.out.println(studentAddress);
+		
+	}
+
+}
