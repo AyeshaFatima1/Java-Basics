d36d3f09debb34f66670e3f60ae06057b7d562e7
AyeshaFatima1 <98936880+AyeshaFatima1@users.noreply.github.com>
2022-09-11 18:41:40 +0530

    Add files via upload

---
+++ 05-02-2022 Java Literals(eg:2)/src/com/javaliterals/Example1.java
@@ -0,0 +1,41 @@
+package com.javaliterals;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//Write a program to print student information(id,name,age,marks,grade,result and address)
+		
+		int studentid=321;
+		String studentname="Ayesha";
+		int studentage=22;
+		float studentmarks=70.0f;
+		char studentgrade='A';
+		boolean studentresult=true;
+		String studentaddress="Hyderabad";
+		
+		System.out.println("student id:"+studentid);
+		//System.out.println(studentid);
+		
+		System.out.print("student name:");
+		System.out.println(studentname);
+		
+		System.out.print("student age:");
+		System.out.println(studentage);
+		
+		System.out.print("student marks:");
+		System.out.println(studentmarks);
+		
+		System.out.print("student grade:");
+		System.out.println(studentgrade);
+		
+		System.out.print("Student result:");
+		System.out.println(studentresult);
+		
+		System.out.print("student address:");
+		System.out.println(studentaddress);
+		
+		
+
+	}
+
+}
