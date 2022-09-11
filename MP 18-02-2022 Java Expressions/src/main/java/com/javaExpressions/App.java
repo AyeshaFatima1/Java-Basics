48ad6134e2453e98b8411a76e99109dc7152fd47
AyeshaFatima1 <98936880+AyeshaFatima1@users.noreply.github.com>
2022-09-11 15:50:13 +0530

    commit
    commit

---
+++ MP 18-02-2022 Java Expressions/src/main/java/com/javaExpressions/App.java
@@ -0,0 +1,11 @@
+package com.javaExpressions;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int num=10;
+    	
+        System.out.println( "Hello World!" );
+    }
+}
