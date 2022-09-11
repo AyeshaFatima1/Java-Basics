3cb83d165b547afd4d4efc0539646f4629ae664e
AyeshaFatima1 <98936880+AyeshaFatima1@users.noreply.github.com>
2022-09-11 18:41:15 +0530

    Add files via upload

---
+++ 03-02-2022 Java Literals/src/com/javaLiterals/Example1.java
@@ -0,0 +1,44 @@
+package com.javaLiterals;
+
+public class Example1
+{
+
+	public static void main(String[] args) 
+	{
+		
+		// write a java program to learner information(id,name,age,address) using java literals
+		 
+        int learnerid=6768;
+        String learnername="Ayesha";
+        int learnerage=22;
+        float learnermarks=85.50f;
+        String learneraddress="Hyderabad";
+        boolean learnerresult=true;
+        char learnergrade='A';
+        
+        System.out.print("Learner id:");
+        System.out.println(learnerid);
+        
+        System.out.print("Learner name:");
+        System.out.println(learnername);
+        
+        System.out.print("Learner age:");
+        System.out.println(learnerage);
+        
+        System.out.print("Learner marks:");
+        System.out.println(learnermarks);
+        
+        System.out.print("Learner address:");
+        System.out.println(learneraddress);
+        
+        System.out.print("Learner result:");
+        System.out.println(learnerresult);
+        
+        System.out.print("Learner grade:");
+        System.out.println(learnergrade);
+        
+        
+        	
+	}
+
+}
